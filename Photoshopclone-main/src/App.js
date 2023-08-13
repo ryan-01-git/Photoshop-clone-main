5b6dfc657b595bc73c9231975181ed2a8d63446d
ryan-01-git <77622772+ryan-01-git@users.noreply.github.com>
2023-08-14 01:06:51 +0530

    Add files via upload

---
+++ Photoshopclone-main/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "./components/login/login";
+import Home from "./components/home/home";
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <>
+        <Navbar/>
+    <Home/>
+        {/* <Login/> */}
+            {/* <Login/> */}
+    {/* hieret */}
+    </>
+  );
+}
+
+export default App;
